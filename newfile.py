537537a8457e734852c4205d828ce06fa8b732cf
chesterlike <123983158+chesterlike@users.noreply.github.com>
2023-01-30 23:34:12 +0500

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,8 +1,10 @@
-a = 2
-b = 2 
+import random
+a = random.randint(0,1)
+b = random.randint(1,2)
 c = a + b
 s = "лох пидр"
 if c == 3 :
     print (s)
-print ("хуй")
-
+else:
+    print ("хуй")
+    
